4423bcc9ce6ece5f57b533e21dc35e01c4868160
samkahchiin <kcsam@siliconjungles.com>
2021-02-03 23:09:41 +0800

    :sparkles: Add title
    Signed-off-by: samkahchiin <kcsam@siliconjungles.com>

--- pages/index.js
+++ pages/index.js
@@ -5,6 +5,14 @@ import Image from 'next/image'
 const IndexPage = () => {
   return (
     <Container style={{ backgroundImage: `url('./background.png')` }}>
+      <img
+        src="/title.png"
+        style={{
+          width: 'auto',
+          height: '25vh',
+          margin: '40px',
+        }}
+      />
       <img src="/buddha-static.png" style={{ width: 'auto', height: '60vh' }} />
     </Container>
   )
@@ -13,8 +21,9 @@ const IndexPage = () => {
 const Container = styled.div`
   display: flex;
   flex-wrap: nowrap;
-  justify-content: center;
-  align-items: flex-end;
+  flex-direction: column;
+  justify-content: flex-end;
+  align-items: center;
 
   background-size: cover;
   background-position: center;
