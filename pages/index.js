f39330c6ea4f9dc08d63a53d8f7158b5ec0b68bc
samkahchiin <kcsam@siliconjungles.com>
2021-02-03 23:00:15 +0800

    :sparkles: Add image to the center
    Signed-off-by: samkahchiin <kcsam@siliconjungles.com>

--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,26 @@
 import React from 'react'
 import styled from 'styled-components'
+import Image from 'next/image'
 
 const IndexPage = () => {
   return (
-    <Container style={{backgroundImage: `url('./background.png')`}} />
+    <Container style={{ backgroundImage: `url('./background.png')` }}>
+      <img src="/buddha-static.png" style={{ width: 'auto', height: '60vh' }} />
+    </Container>
   )
 }
 
 const Container = styled.div`
   display: flex;
+  flex-wrap: nowrap;
   justify-content: center;
-  align-items: center;
-  font-size: 50px;
+  align-items: flex-end;
+
   background-size: cover;
   background-position: center;
   background-repeat: no-repeat;
   height: 100vh;
+  overflow: hidden;
 `
 
 export default IndexPage
