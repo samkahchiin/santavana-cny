10a8754c12b34cc2f8f99d03cf4f5149d04c6688
samkahchiin <kcsam@siliconjungles.com>
2021-02-05 12:57:07 +0800

    :bug: Remove tappable
    Signed-off-by: samkahchiin <kcsam@siliconjungles.com>

--- src/components/shared/Button.js
+++ src/components/shared/Button.js
@@ -1,15 +1,13 @@
-import Tappable from 'react-tappable/lib/Tappable'
 import classes from './Button.module.css'
 
 const Button = ({ btnText, btnStyle = {}, className = [], onClick }) => (
-  <Tappable onTap={onClick}>
-    <button
-      className={`${classes.Button} ${className.join(' ')}`}
-      style={btnStyle}
-    >
-      {btnText}
-    </button>
-  </Tappable>
+  <button
+    onClick={onClick}
+    className={`${classes.Button} ${className.join(' ')}`}
+    style={btnStyle}
+  >
+    {btnText}
+  </button>
 )
 
 export default Button
