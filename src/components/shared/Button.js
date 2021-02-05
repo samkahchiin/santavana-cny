3ae3907016bbcfb7eecfa7205432c9950c529c7c
samkahchiin <kcsam@siliconjungles.com>
2021-02-05 10:32:26 +0800

    :sparkles: Add Tappable
    Signed-off-by: samkahchiin <kcsam@siliconjungles.com>

--- src/components/shared/Button.js
+++ src/components/shared/Button.js
@@ -1,24 +1,33 @@
+import Tappable from 'react-tappable/lib/Tappable'
+
 const PRIMARY_COLOR = '#E0A91A'
 const SECONDARY_COLOR = '#FFF6E0'
 const TERTIARY_COLOR = '#695237'
 
 const Button = ({ btnText, btnStyle = {}, onClick }) => (
-  <button
-    onClick={onClick}
-    style={{
-      ...btnStyle,
+  <Tappable onTap={onClick}>
+    <button
+      style={{
+        ...btnStyle,
 
-      padding: '5pt',
-      fontSize: '14pt',
-      borderRadius: '15pt',
-      cursor: 'pointer',
-      border: `solid 1.5pt ${SECONDARY_COLOR}`,
-      backgroundColor: PRIMARY_COLOR,
-      color: SECONDARY_COLOR,
-    }}
-  >
-    {btnText}
-  </button>
+        padding: '5pt',
+        fontSize: '14pt',
+        borderRadius: '15pt',
+        cursor: 'pointer',
+        border: `solid 1.5pt ${SECONDARY_COLOR}`,
+        backgroundColor: PRIMARY_COLOR,
+        color: SECONDARY_COLOR,
+
+        '&hover': {
+          border: `solid 1.5pt ${PRIMARY_COLOR}`,
+          backgroundColor: SECONDARY_COLOR,
+          color: PRIMARY_COLOR,
+        },
+      }}
+    >
+      {btnText}
+    </button>
+  </Tappable>
 )
 
 export default Button
