40695c56af423b6f7010aab4217e9e58c9dcc30c
samkahchiin <kcsam@siliconjungles.com>
2021-02-05 11:36:11 +0800

    :sparkles: Update button css
    Signed-off-by: samkahchiin <kcsam@siliconjungles.com>

--- src/components/shared/Button.js
+++ src/components/shared/Button.js
@@ -1,30 +1,9 @@
 import Tappable from 'react-tappable/lib/Tappable'
-
-const PRIMARY_COLOR = '#E0A91A'
-const SECONDARY_COLOR = '#FFF6E0'
-const TERTIARY_COLOR = '#695237'
+import classes from './Button.module.css'
 
 const Button = ({ btnText, btnStyle = {}, onClick }) => (
   <Tappable onTap={onClick}>
-    <button
-      style={{
-        ...btnStyle,
-
-        padding: '5pt',
-        fontSize: '14pt',
-        borderRadius: '15pt',
-        cursor: 'pointer',
-        border: `solid 1.5pt ${SECONDARY_COLOR}`,
-        backgroundColor: PRIMARY_COLOR,
-        color: SECONDARY_COLOR,
-
-        '&hover': {
-          border: `solid 1.5pt ${PRIMARY_COLOR}`,
-          backgroundColor: SECONDARY_COLOR,
-          color: PRIMARY_COLOR,
-        },
-      }}
-    >
+    <button className={classes.Button} style={btnStyle}>
       {btnText}
     </button>
   </Tappable>
