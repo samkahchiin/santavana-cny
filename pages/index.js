2e04f5c4121bdabe071d79f66e88f1e1237beb0e
samcarmen <carmen.samkahman@gmail.com>
2021-02-09 12:42:09 +0800

    Add cny music

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,17 @@
 import React from 'react'
 import Button from '../src/components/shared/Button'
 import Steps from '../src/components/Steps'
+import ReactAudioPlayer from 'react-audio-player'
 
-const IndexPage = () => <Steps />
+const IndexPage = () => {
+    return (
+       <>
+        <Steps />
+        <div>
+            <ReactAudioPlayer useRef='audio_tag' src="/cny_music.mp3" autoPlay/>
+        </div> 
+    </>
+    )
+}
 
 export default IndexPage
