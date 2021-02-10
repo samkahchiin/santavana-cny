64c1187860b030cd67111387f54287b03a94a81e
samkahchiin <kcsam@siliconjungles.com>
2021-02-10 20:00:51 +0800

    :recycle: Remove redundant div
    Signed-off-by: samkahchiin <kcsam@siliconjungles.com>

--- pages/index.js
+++ pages/index.js
@@ -3,15 +3,11 @@ import Button from '../src/components/shared/Button'
 import Steps from '../src/components/Steps'
 import ReactAudioPlayer from 'react-audio-player'
 
-const IndexPage = () => {
-    return (
-       <>
-        <Steps />
-        <div>
-            <ReactAudioPlayer src="/cny_music.mp3" autoPlay loop/>
-        </div> 
-    </>
-    )
-}
+const IndexPage = () => (
+  <>
+    <Steps />
+    <ReactAudioPlayer src="/cny_music.mp3" autoPlay loop />
+  </>
+)
 
 export default IndexPage
