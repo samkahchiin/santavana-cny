bfa1d0ac04fa8293c9b879f591157c8dac02aa6a
samcarmen <carmen.samkahman@gmail.com>
2021-02-10 14:24:09 +0800

    Change to react-h5-audio-player

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,18 @@
 import React from 'react'
 import Button from '../src/components/shared/Button'
 import Steps from '../src/components/Steps'
-import ReactAudioPlayer from 'react-audio-player'
+import AudioPlayer from 'react-h5-audio-player';
 
 const IndexPage = () => {
     return (
        <>
         <Steps />
-        <div>
-            <ReactAudioPlayer src="/cny_music.mp3" autoPlay loop/>
-        </div> 
-    </>
+        <AudioPlayer
+            autoPlay
+            src="cny_music.mp3"
+            onPlay={e => console.log("onPlay")}
+        />
+    </> 
     )
 }
 
