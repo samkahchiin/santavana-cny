439d491b8f9108f83df2a1a19b01f22f6954215d
samkahchiin <kcsam@siliconjungles.com>
2021-02-10 21:34:20 +0800

    :sparkles: Add react fullscreen
    Signed-off-by: samkahchiin <kcsam@siliconjungles.com>

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Button from '../src/components/shared/Button'
 import Steps from '../src/components/Steps'
 import ReactAudioPlayer from 'react-audio-player'
+import { FullScreen, useFullScreenHandle } from 'react-full-screen'
 
-const IndexPage = () => (
-  <>
-    <Steps />
-    <ReactAudioPlayer src="/cny_music.mp3" autoPlay loop />
-  </>
-)
+const IndexPage = () => {
+  const handle = useFullScreenHandle()
+
+  useEffect(() => {
+    handle.enter
+  }, [])
+
+  return (
+    <FullScreen handle={handle}>
+      <Steps />
+      <ReactAudioPlayer src="/cny_music.mp3" autoPlay loop />
+    </FullScreen>
+  )
+}
 
 export default IndexPage
